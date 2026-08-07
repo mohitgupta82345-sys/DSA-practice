e6a20731edc164ce0794d229f91e7d5927356ea4
Mohit Gupta <mohit.gupta82345@gmail.com>
2026-08-07 23:26:03 +0530

    Updated sum_array.java

--- sum_array.java
+++ sum_array.java
@@ -1,7 +1,7 @@
 public class sum_array {
     public static void main(String[] args ){
         int sum =0;
-
+// git practice
         int arr[] = {1,2,3,4,5};
 for(int i=0; i<=4; i++){
     sum+=arr[i];
